460c60cf519efebff435eec825235bf4f88290c2
Szymon <szy.rudnicki@gmail.com>
2016-10-23 01:32:29 +0200

    mvp started, not tested

---
+++ app/src/main/java/pl/temomuko/moreflow/wikiofthrones/data/DataManager.java
@@ -0,0 +1,8 @@
+package pl.temomuko.moreflow.wikiofthrones.data;
+
+/**
+ * Created by Szymon on 23.10.2016.
+ */
+
+public class DataManager {
+}
---
+++ app/src/main/java/pl/temomuko/moreflow/wikiofthrones/ui/splash/SplashActivity.java
@@ -0,0 +1,16 @@
+package pl.temomuko.moreflow.wikiofthrones.ui.splash;
+
+import pl.temomuko.moreflow.wikiofthrones.R;
+import pl.temomuko.moreflow.wikiofthrones.ui.SuperActivity;
+
+/**
+ * Created by Szymon on 23.10.2016.
+ */
+
+public class SplashActivity extends SuperActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_splash;
+    }
+}
